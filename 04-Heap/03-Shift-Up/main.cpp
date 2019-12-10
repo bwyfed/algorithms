b911aa6a661ccab21bd34166c2609e78756ac9b7
Bianwy <bianwy025@qq.com>
2019-12-10 10:31:26 +0800

    实现从堆中删除元素的操作；重新整理项目文件。

---
+++ 04-Heap/03-Shift-Up/main.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <cassert>
+#include <ctime>
+
+using namespace std;
+
+template<typename Item>
+class MaxHeap {
+private:
+	Item *data;
+	int count; // 堆中有多少个元素
+	int capacity;
+
+	void shiftUp(int k) {
+		while (k > 1 && data[k / 2] < data[k]) {
+			swap(data[k / 2], data[k]);
+			k /= 2;
+		}
+	}
+	void shiftDown(int k) {
+		while (2 * k <= count) { // 判断是否有子结点
+			int j = 2 * k; // 在此轮循环中，data[k]和data[j]交换位置
+			if (j + 1 <= count && data[j + 1] > data[j]) // 判断右孩子和左孩子的大小关系
+				j+=1;
+			if (data[k] >= data[j])
+				break;
+			swap(data[k], data[j]);
+			k = j;
+		}
+	}
+public:
+	MaxHeap(int capacity) {
+		data = new Item[capacity + 1];
+		count = 0;
+		this->capacity = capacity;
+	}
+	~MaxHeap() {
+		delete[] data;
+	}
+	int size() {
+		return count;
+	}
+	bool isEmpty() {
+		return count == 0;
+	}
+	void insert(Item item) {
+		assert(count + 1 <= capacity);
+		data[count + 1] = item;
+		count++;
+		shiftUp(count);
+	}
+	Item extractMax() {
+		assert(count > 0);
+		Item ret = data[1];
+
+		swap(data[1], data[count]);
+		count--;
+		shiftDown(1);
+		return ret;
+	}
+};
+int main() {
+	MaxHeap<int> maxheap = MaxHeap<int>(100);
+//	cout << maxheap.size() << endl;
+
+	srand(time(NULL));
+	for (int i = 0; i < 15; i++)
+		maxheap.insert(rand() % 100);
+
+	while (!maxheap.isEmpty())
+		cout << maxheap.extractMax() << " ";
+	cout << endl;
+	return 0;
+}
