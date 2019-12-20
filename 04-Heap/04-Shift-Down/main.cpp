63699fbbb4546c08c67f7d502c265f41451a41b0
Bianwy <bianwy025@qq.com>
2019-12-20 12:13:04 +0800

    整理代码。

--- 04-Heap/04-Shift-Down/main.cpp
+++ 04-Heap/04-Shift-Down/main.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 #include <ctime>
 #include <cassert>
-#include <algorithm>
-#include <string>
 using namespace std;
 
 template<typename Item>
--- 04-Heap/05-Heapify/main.cpp
+++ 04-Heap/05-Heapify/main.cpp
@@ -1,26 +1,74 @@
 #include <iostream>
-#include "Heap.h"
-#include "SortTestHelper.h"
-
+#include <ctime>
+#include <cassert>
+#include <algorithm>
+#include <string>
 using namespace std;
 
-template<typename T>
-void heapSort1(T arr[], int n) {
-	MaxHeap<T> maxheap = MaxHeap<T>(n);
-	for (int i = 0; i < n; i++)
-		maxheap.insert(arr[i]);
-	for (int i = n - 1; i >= 0; i--)
-		arr[i] = maxheap.extractMax();
-}
+template<typename Item>
+class MaxHeap {
+private:
+	Item *data;
+	int count; // 堆中有多少个元素
+	int capacity;
+	// 将k索引位置的元素向上移动，维持堆的定义
+	void shiftUp(int k) {
+		while (k > 1 && data[k / 2] < data[k]) {
+			swap(data[k / 2], data[k]);
+			k /= 2;
+		}
+	}
+	void shiftDown(int k) {
+		while ( 2*k <= count ) {
+			int j = 2 * k; // 在此轮循环中，data[k]和data[j]交换位置
+			if (j + 1 <= count && data[j + 1] > data[j])
+				j += 1;
+			if (data[k] >= data[j])
+				break;
+			swap(data[k], data[j]);
+			k = j;
+		}
+	}
+public:
+	MaxHeap(int capacity) {
+		data = new Item[capacity + 1];
+		count = 0;
+		this->capacity = capacity;
+	}
+	~MaxHeap() {
+		delete[] data;
+	}
+	int size() {
+		return count;
+	}
+	bool isEmpty() {
+		return count == 0;
+	}
+	void insert(Item item) {
+		assert(count + 1 <= capacity);
+		data[count + 1] = item;
+		count++;
+		shiftUp(count);
+	}
+	Item extractMax() {
+		assert(count > 0);
+		Item ret = data[1];
+		swap(data[1], data[count]);
+		count--;
+		shiftDown(1);
+		return ret;
+	}
+};
 int main() {
-	int n = 1000000;
-	int* arr1 = SortTestHelper::generateRandomArray(n, 0, n);
-	// 测试1: 一般性测试
-	cout << "Test for Random Array, size = " << n << ", random range[0, " << n << "]" << endl;
-	SortTestHelper::testSort("Heap Sort 1", heapSort1, arr1, n);
+	MaxHeap<int> maxheap = MaxHeap<int>(100);
+//	cout << maxheap.size() << endl;
 
-	delete[] arr1;
-
+	srand(time(NULL));
+	for (int i = 0; i < 50; i++)
+		maxheap.insert(rand() % 100);
+	while (!maxheap.isEmpty())
+		cout << maxheap.extractMax() << " ";
 	cout << endl;
+
 	return 0;
 }
