e437330c2d1e3c0347409c1c65dae15adeb1c63e
Bianwy <bianwy025@qq.com>
2019-12-24 15:08:51 +0800

    实现二分搜索树的查找功能，包括contain和search。

---
+++ 05-Binary-Search-Tree/04-Binary-Search-Tree-Search/main.cpp
@@ -0,0 +1,148 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <ctime>
+#include "FileOps.h"
+#include "SequenceST.h"
+
+using namespace std;
+
+template<typename Key, typename Value>
+class BST {
+private:
+	struct Node
+	{
+		Key key;
+		Value value;
+		Node *left;
+		Node *right;
+
+		Node(Key key, Value value) {
+			this->key = key;
+			this->value = value;
+			this->left = this->right = NULL;
+		}
+	};
+	Node *root;
+	int count; // BST有多少个结点
+public:
+	BST() {
+		root = NULL;
+		count = 0;
+	}
+	~BST() {
+		// TODO: ~BST()
+	}
+	int size() {
+		return count;
+	}
+	bool isEmpty() {
+		return count == 0;
+	}
+
+	void insert(Key key, Value value) {
+		root = insert(root, key, value);
+	}
+
+	bool contain(Key key) {
+		return contain(root, key);
+	}
+	Value* search(Key key) {
+		return search(root, key);
+	}
+private:
+	// 向以node为根的二叉搜索树中，插入结点(key, value)
+	// 返回插入新结点后的二叉搜索树的根
+	Node* insert(Node *node, Key key, Value value) {
+		if (node == NULL) {
+			count++;
+			return new Node(key, value);
+		}
+		if (key == node->key)
+			node->value = value;
+		else if (key < node->key)
+			node->left = insert(node->left, key, value);
+		else // key > node->key
+			node->right = insert(node->right, key, value);
+		return node;
+	}
+	// 查看以node为根的二叉搜索树中是否包含键值为key的节点
+	bool contain(Node *node, Key key) {
+		if (node == NULL)
+			return false;
+
+		if (key == node->key)
+			return true;
+		else if (key < node->key)
+			return contain(node->left, key);
+		else // key > node->key
+			return contain(node->right, key);
+	}
+	// 在以node为根的二叉搜索树中查找key所对应的value
+	Value* search(Node* node, Key key) {
+		if (node == NULL)
+			return NULL;
+
+		if (key == node->key)
+			return &(node->value);
+		else if (key < node->key)
+			return search(node->left, key);
+		else // key > node->key
+			return search(node->right, key);
+	}
+};
+int main() {
+	// 使用圣经作为我们的测试用例
+	string filename = "bible.txt";
+	vector<string> words;
+	if (FileOps::readFile(filename, words)) {
+		cout << "There are totally " << words.size() << " words in " << filename << endl;
+		cout << endl;
+
+		// 测试 BST
+		time_t startTime = clock();
+		// 统计圣经中所有词的词频
+		// 注: 这个词频统计法相对简陋, 没有考虑很多文本处理中的特殊问题
+		// 在这里只做性能测试用
+		BST<string, int> bst = BST<string, int>();
+		for (vector<string>::iterator iter = words.begin(); iter != words.end(); iter++) {
+			int *res = bst.search(*iter);
+			if (res == NULL)
+				bst.insert(*iter, 1);
+			else
+				(*res)++;
+		}
+		// 输出圣经中god一词出现的频率
+		if (bst.contain("god"))
+			cout << "'god' : " << *bst.search("god") << endl;
+		else
+			cout << "No word 'god' in " << filename << endl;
+		time_t endTime = clock();
+		cout << "BST, time: " << double(endTime - startTime) / CLOCKS_PER_SEC << " s." << endl;
+		cout << endl;
+
+		// 测试顺序查找表 SST
+		startTime = clock();
+
+		// 统计圣经中所有词的词频
+		// 注: 这个词频统计法相对简陋, 没有考虑很多文本处理中的特殊问题
+		// 在这里只做性能测试用
+		SequenceST<string, int> sst = SequenceST<string, int>();
+		for (vector<string>::iterator iter = words.begin(); iter != words.end(); iter++) {
+			int *res = sst.search(*iter);
+			if (res == NULL)
+				sst.insert(*iter, 1);
+			else
+				(*res)++;
+		}
+		// 输出圣经中god一词出现的频率
+		if (sst.contain("god"))
+			cout << "'god' : " << *sst.search("god") << endl;
+		else
+			cout << "No word 'god' in " << filename << endl;
+		endTime = clock();
+		cout << "SST , time: " << double(endTime - startTime) / CLOCKS_PER_SEC << " s." << endl;
+	}
+
+	return 0;
+}
